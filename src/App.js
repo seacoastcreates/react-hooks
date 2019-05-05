45c36c340d4e5cdeb0c71f26c4b26887fd3421a0
Kirby Robinson <krobinson@mdconnectinc.com>
2019-05-05 15:35:30 -0400

    form submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components'
+import Toggle from './Toggle'
 
-function App() {
+const App = () => {
+
+  // useState
+  // declare const as an array: const [] 
+  // array contains input value itself and the set value: const [value, setValue]
+  // const is equal to useState function: const [value, setValue] = useState();
+  // useState() accepts initialState as param: useState(initialState);
+  // hence: const [value, setValue] = useState(initialState);
+
+  const [name, setName] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Wrapper>
+        <header>
+          <h1>react hooks</h1>
+        </header>
+        <main>
+
+
+          <Toggle />
+          <form onSubmit = { e => {
+            e.preventDefault();
+            formSubmit();
+          }}>
+            <input 
+              type="text" 
+              onChange={ e => setName(e.target.value)} 
+              value={name} 
+            />
+            <button>Submit</button>
+          </form>
+        
+        
+        
+        
+        </main>
+        <footer></footer>
+      </Wrapper>
     </div>
   );
 }
+
+const formSubmit = () => {
+  console.log('email sent!');
+}
+
+const Wrapper = styled.div `
+  padding: 0 20px;
+`
 
 export default App;
---
+++ src/Toggle.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const Toggle = () => {
+
+  const [isToggled, setToggle] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setToggle(!isToggled)}>Toggle</button>
+      {isToggled && <h2>hello</h2>}
+    </div>
+  )
+}
+
+export default Toggle
+
+
+// import React, { Component } from 'react';
+
+// export default class Toggle extends Component {
+//   state = {
+//     isToggled: false
+//   };
+
+//   toggle = () => {
+//     this.setState(state => {
+//       return { isToggled: !state.isToggle };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick{this.toggle}>Toggle</button>
+//         {this.state.isToggled && <h2>hello</h2>}
+//       </div>
+//     );
+//   }
+// }
